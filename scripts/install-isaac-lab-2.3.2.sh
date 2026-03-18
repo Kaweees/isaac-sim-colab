dec7309a97e7de8370f4e6c503cec9dbe438d114
Miguel Villa Floran <miguel.villafloran@gmail.com>
2026-03-18 09:24:39 -0700

    Update Issac Lab version

---
+++ scripts/install-isaac-lab-2.3.2.sh
@@ -0,0 +1,21 @@
+#! /bin/bash -e
+
+# This script is intended to be used with a specially modified Colab notebook.
+# i.e., https://colab.research.google.com/github/j3soon/isaac-sim-colab/blob/main/notebooks/isaac-lab-2.3.2-colab.ipynb
+# For more information, see: https://github.com/j3soon/isaac-sim-colab.
+
+# Confirm that Isaac Sim is installed
+which isaacsim
+
+# Install Isaac Lab through pip.
+# Ref: https://isaac-sim.github.io/IsaacLab/main/source/setup/installation/pip_installation.html
+apt-get install -y cmake build-essential
+uv pip install -qq "isaaclab[all,isaacsim]>=2.3.2" --extra-index-url https://pypi.nvidia.com
+
+# Set environment variables
+# Ref: https://docs.isaacsim.omniverse.nvidia.com/latest/installation/install_python.html#running-isaac-sim
+export OMNI_KIT_ACCEPT_EULA=YES
+
+# Run minimal example
+# Ref: https://isaac-sim.github.io/IsaacLab/main/source/overview/reinforcement-learning/rl_existing_scripts.html
+# python -m isaaclab.train --task Isaac-Cartpole-v0 --headless
---
+++ scripts/install-isaac-sim-5.1.sh
@@ -0,0 +1,79 @@
+#! /bin/bash -e
+
+# This script is intended to be used with a specially modified Colab notebook.
+# i.e., https://colab.research.google.com/github/j3soon/isaac-sim-colab/blob/main/notebooks/isaac-sim-5.1-colab.ipynb
+# For more information, see: https://github.com/j3soon/isaac-sim-colab.
+
+# Download and execute Python 3.10 set up script
+# Ref: https://github.com/j3soon/colab-python-version
+wget -O py310.sh https://raw.githubusercontent.com/j3soon/colab-python-version/main/scripts/py310.sh
+bash py310.sh
+
+# Set up Vulkan runtime dependencies
+apt-get install -y vulkan-tools
+# Ref: https://askubuntu.com/a/575808
+apt-get install -y libglu1
+# Ref: https://stackoverflow.com/a/72736003
+conda install -y gcc=12.1.0 -c conda-forge
+# Ref: https://docs.isaacsim.omniverse.nvidia.com/latest/installation/install_python.html
+ldd --version
+# Ref: https://github.com/j3soon/docker-vulkan-runtime
+cat > /etc/vulkan/icd.d/nvidia_icd.json <<EOF
+{
+    "file_format_version" : "1.0.0",
+    "ICD": {
+        "library_path": "libGLX_nvidia.so.0",
+        "api_version" : "1.3.194"
+    }
+}
+EOF
+mkdir -p /usr/share/glvnd/egl_vendor.d && \
+    cat > /usr/share/glvnd/egl_vendor.d/10_nvidia.json <<EOF
+{
+    "file_format_version" : "1.0.0",
+    "ICD" : {
+        "library_path" : "libEGL_nvidia.so.0"
+    }
+}
+EOF
+cat > /etc/vulkan/implicit_layer.d/nvidia_layers.json <<EOF
+{
+    "file_format_version" : "1.0.0",
+    "layer": {
+        "name": "VK_LAYER_NV_optimus",
+        "type": "INSTANCE",
+        "library_path": "libGLX_nvidia.so.0",
+        "api_version" : "1.3.194",
+        "implementation_version" : "1",
+        "description" : "NVIDIA Optimus layer",
+        "functions": {
+            "vkGetInstanceProcAddr": "vk_optimusGetInstanceProcAddr",
+            "vkGetDeviceProcAddr": "vk_optimusGetDeviceProcAddr"
+        },
+        "enable_environment": {
+            "__NV_PRIME_RENDER_OFFLOAD": "1"
+        },
+        "disable_environment": {
+            "DISABLE_LAYER_NV_OPTIMUS_1": ""
+        }
+    }
+}
+EOF
+vulkaninfo --summary
+
+# Install uv for faster pip install
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Install Isaac Sim through pip.
+# Ref: https://docs.isaacsim.omniverse.nvidia.com/latest/installation/install_python.html
+uv pip install -qq "isaacsim[all,extscache]>=5.1.0.0" --extra-index-url https://pypi.nvidia.com
+
+# Download Isaac Sim minimal example
+wget -O time_stepping.py https://raw.githubusercontent.com/j3soon/isaac-sim-colab/refs/heads/main/thirdparty/isaacsim/standalone_examples/api/isaacsim.core.api/time_stepping.py
+
+# Set environment variables
+# Ref: https://docs.isaacsim.omniverse.nvidia.com/latest/installation/install_python.html#running-isaac-sim
+export OMNI_KIT_ACCEPT_EULA=YES
+
+# Run minimal example to test installation
+# python time_stepping.py
